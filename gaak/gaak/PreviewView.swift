8ae249beddb78ff13953e61b40ff8092a8945b36
DongHyunTedKim <53430924+DongHyunTedKim@users.noreply.github.com>
2020-09-14 16:32:34 +0900

    camera basic function outline
    카메라 기본기능을 위해 outline 작성

---
+++ gaak/gaak/PreviewView.swift
@@ -0,0 +1,37 @@
+//
+//  PreviewView.swift
+//  gaak
+//
+//  Created by Ted Kim on 2020/09/14.
+//  Copyright © 2020 Ted Kim. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PreviewView: UIView {
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        guard let layer = layer as? AVCaptureVideoPreviewLayer else {
+            fatalError("Expected `AVCaptureVideoPreviewLayer` type for layer. Check PreviewView.layerClass implementation.")
+        }
+        
+        layer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        layer.connection?.videoOrientation = .portrait
+        return layer
+    }
+    
+    var session: AVCaptureSession? {
+        get {
+            return videoPreviewLayer.session
+        }
+        set {
+            videoPreviewLayer.session = newValue
+        }
+    }
+    
+    // MARK: UIView
+    
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+}
