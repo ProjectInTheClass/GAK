595eb4d48aa33e8c13951fe3a39a3b2198922726
DongHyunTedKim <53430924+DongHyunTedKim@users.noreply.github.com>
2020-10-04 10:39:16 +0900

    week9_sandbox_upload

---
+++ Sandbox/week9_practice/week9_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week9_practice
+//
+//  Created by Ted Kim on 2020/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
