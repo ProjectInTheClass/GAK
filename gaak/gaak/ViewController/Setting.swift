d28ac7af1917c7b07b116ee05b612416da5e5551
SEOINJAE <69412493+SEOINJAE@users.noreply.github.com>
2020-11-06 00:00:50 +0900

    중간저장
    내일 이어서 하기 위한 중간저장

---
+++ gaak/gaak/ViewController/Setting.swift
@@ -0,0 +1,17 @@
+//
+//  Setting.swift
+//  gaak
+//
+//  Created by 서인재 on 2020/11/05.
+//  Copyright © 2020 Ted Kim. All rights reserved.
+//
+
+import Foundation
+
+struct Setting {
+    var content: String
+    
+    init(content: String) {
+        self.content = content
+    }
+}
---
+++ gaak/gaak/ViewController/SettingTableViewController.swift
@@ -0,0 +1,98 @@
+//
+//  SettingTableViewController.swift
+//  gaak
+//
+//  Created by 서인재 on 2020/11/05.
+//  Copyright © 2020 Ted Kim. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewController: UITableViewController {
+
+    
+    var settings: [Setting] = [
+        Setting(content: "개인정보처리방침"),
+        Setting(content: "오픈소스 라이선스"),
+        Setting(content: "버전")
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return settings.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingCell", for: indexPath)
+
+        let setting = settings[indexPath.row]
+        cell.textLabel?.text = "\(setting.content)"
+
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
