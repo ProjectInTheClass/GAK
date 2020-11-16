8351ee8489ae6a1caf07fd376c12058a09739735
SEOINJAE <69412493+SEOINJAE@users.noreply.github.com>
2020-11-16 21:01:26 +0900

    햅틱 토글버튼 완성
    동작 메소드를 추가해야함

--- gaak/gaak/ViewController/SettingTableViewController.swift
+++ gaak/gaak/ViewController/SettingTableViewController.swift
@@ -45,39 +45,50 @@ class SettingTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case 0:
             return settings.count
-
+            
         case 1:
             return HapticCellIdentifier.count
+            
         default:
-            return 1
+            return 0
         }
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if indexPath.section < 1 {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingCell", for: indexPath)
-
-        let setting = settings[indexPath.row]
-        cell.textLabel?.text = "\(setting.content)"
-        
-        return cell
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SettingCell", for: indexPath)
+            
+            let setting = settings[indexPath.row]
+            cell.textLabel?.text = "\(setting.content)"
+            
+            return cell
+            
+        } else if indexPath.section == 1{
+            let HapticCell: HapticCellTableViewCell = tableView.dequeueReusableCell(withIdentifier: "HapticCell", for: indexPath) as! HapticCellTableViewCell
+            
+            let Haptic = HapticCellIdentifier[indexPath.row]
+            HapticCell.leftLabel?.text = "\(Haptic)"
+            HapticCell.selectionStyle = .none
+            
+            return HapticCell
             
         } else {
             let HapticCell: HapticCellTableViewCell = tableView.dequeueReusableCell(withIdentifier: "HapticCell", for: indexPath) as! HapticCellTableViewCell
             
             let Haptic = HapticCellIdentifier[indexPath.row]
             HapticCell.leftLabel?.text = "\(Haptic)"
+            HapticCell.selectionStyle = .none
             
             return HapticCell
         }
@@ -88,30 +99,32 @@ class SettingTableViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
         
-        switch indexPath.row {
-        
-        case 0:
-            if let url = URL(string: "https://codershigh.github.io/WebSite/#/") {
-            UIApplication.shared.open(url)
-            }
+        if indexPath.section == 0{
+            switch indexPath.row{
             
-        case 1:
-            if let url = URL(string: "https://www.notion.so/gaak/b7bb2b4c005b48eb9bc08406116e1041") {
-            UIApplication.shared.open(url)
+            case 0:
+                if let url = URL(string: "https://codershigh.github.io/WebSite/#/") {
+                    UIApplication.shared.open(url)
+                }
+                
+            case 1:
+                if let url = URL(string: "https://www.notion.so/gaak/b7bb2b4c005b48eb9bc08406116e1041") {
+                    UIApplication.shared.open(url)
+                }
+                
+            case 2:
+                if let url = URL(string: "https://www.notion.so/gaak/Gaak-023f9aefb51747a0807e861ea527b68c") {
+                    UIApplication.shared.open(url)
+                }
+                
+            case 3:
+                if let url = URL(string: "https://www.notion.so/gaak/425cfe05d58f4afdba9e39f3673db926") {
+                    UIApplication.shared.open(url)
+                }
+                
+            default:
+                return
             }
-            
-        case 2:
-            if let url = URL(string: "https://www.notion.so/gaak/Gaak-023f9aefb51747a0807e861ea527b68c") {
-            UIApplication.shared.open(url)
-            }
-            
-        case 3:
-            if let url = URL(string: "https://www.notion.so/gaak/425cfe05d58f4afdba9e39f3673db926") {
-            UIApplication.shared.open(url)
-            }
-            
-        default:
-            return
         }
     }
 }
